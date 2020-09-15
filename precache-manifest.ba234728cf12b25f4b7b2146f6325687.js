a0edd718760122d1aa5111ca67c6248365a99eb0
RadioJackie <jacobpeters628@gmail.com>
2020-09-15 14:24:28 -0700

    Updates

---
+++ precache-manifest.ba234728cf12b25f4b7b2146f6325687.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77264989232e4f7fd559ba3449bdb71b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "544884ba96b16b409f1c",
+    "url": "/React-Portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "ff9a7f7456bf61ed12cd",
+    "url": "/React-Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "544884ba96b16b409f1c",
+    "url": "/React-Portfolio/static/js/2.2df1a437.chunk.js"
+  },
+  {
+    "revision": "d39714e1b941a480462aecfa296c1c20",
+    "url": "/React-Portfolio/static/js/2.2df1a437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff9a7f7456bf61ed12cd",
+    "url": "/React-Portfolio/static/js/main.8f2e8afe.chunk.js"
+  },
+  {
+    "revision": "77241438cbc540ecb316",
+    "url": "/React-Portfolio/static/js/runtime-main.08263ca6.js"
+  },
+  {
+    "revision": "1f8e0e5d3191dfe8ab39204c582ef7be",
+    "url": "/React-Portfolio/static/media/BudgetTracker.1f8e0e5d.jpg"
+  },
+  {
+    "revision": "b8f6a8b8a5bea2d7082870e723102d1d",
+    "url": "/React-Portfolio/static/media/FitnessTracker.b8f6a8b8.jpg"
+  },
+  {
+    "revision": "6745380e75fec8fa519635b39d0deebe",
+    "url": "/React-Portfolio/static/media/NoteTaker.6745380e.jpg"
+  },
+  {
+    "revision": "a0ebb3a0c75aa6c05d81bbe03f589050",
+    "url": "/React-Portfolio/static/media/Profilepic.a0ebb3a0.jpg"
+  },
+  {
+    "revision": "dc2b46a0c11062cef29ec72395a3b4d5",
+    "url": "/React-Portfolio/static/media/Resume.dc2b46a0.jpg"
+  },
+  {
+    "revision": "1c80b0376291f82391be27f07681b8ce",
+    "url": "/React-Portfolio/static/media/TukTuk.1c80b037.jpg"
+  },
+  {
+    "revision": "5b3f1e7c258325bebd7826688d3ad69f",
+    "url": "/React-Portfolio/static/media/WhatsForDinner.5b3f1e7c.jpg"
+  }
+]);
